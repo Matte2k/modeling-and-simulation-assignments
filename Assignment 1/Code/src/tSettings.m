9370f7f640f41fd3c45b0d79143c1087427a946a
Matte2k <151649279+Matte2k@users.noreply.github.com>
2024-04-05 11:24:05 +0200

    Theta pack update to final release
    List of changes:
    - add function value calculation at each step
    - add implicit flag in the 'info' struct to check implicity of the method
    - add comment and descriptions to all functions
    - add default settings for the settings function
    - general bug fix
    - general cosmetic fix

--- Assignment 1/Code/src/tSettings.m
+++ Assignment 1/Code/src/tSettings.m
@@ -1,20 +1,67 @@
-function [tOptions] = tSettings(theta,method,submethod)
-%TSETTINGS Summary of this function goes here
-%   Detailed explanation goes here
+function [tOptions] = tSettings(theta,method,mode,toll,nmax)
+%T SETTINGS - Create the options struct for theta method
+%
+%   Syntax:
+%       [tOptions] = tSettings(theta,method,mode,toll,nmax)
+%
+%   Input:
+%       theta(*),       double:  define the 'theta' parameter of the theta methods
+%       method(*),        char:  define the solver (available 'fsolve', 'fzero' and 'newton')
+%       mode:             char:  set how to compute jacobian (needed only with 'newton')
+%       toll:           double:  set tollerance in newton method (needed only with 'newton')
+%       nmax:           double:  set max number of iterations in newton method (needed only with 'newton')
+%   
+%   Output:
+%       tOptions,  struct:  contains settings for IEX4 method:
+%           - tOptions.theta = theta
+%           - tOptions.method = method
+%           - tOptions.submethod.mode = mode (present only if method='newton')
+%           - tOptions.submethod.toll = toll (present only if method='newton')
+%           - tOptions.submethod.nmax = nmax (present only if method='newton')
+%
+%   Default settings for optional input (*):
+%       theta:   set as '0.5' by default inside theta function
+%       method:  set as 'fsolve' by default inside theta function
+%       mode:    set as default value inside netwon.m
+%       toll:    set as default value inside netwon.m
+%       nmax:    set as default value inside netwon.m
+%
 
-if isempty(submethod)
-    submethod.mode = [];
-    submethod.toll = [];
-    submethod.nmax = [];
-end
 
-tOptions = struct;
-    tOptions.theta = theta;
-    tOptions.method = method;
-    tOptions.submethod = struct;
-        tOptions.submethod.mode = submethod.mode;
-        tOptions.submethod.toll = submethod.toll;
-        tOptions.submethod.nmax = submethod.nmax;
+    %%% Default value for optional input
+    if nargin < 5
+        nmax = [];
+        if nargin < 4
+            toll = [];
+            if nargin < 3
+                mode = [];
+                if nargin < 2
+                    method = [];
+                    if nargin < 1
+                        theta = [];
+                    end
+                end
+            end
+        end
+    end
+
+
+    %%% Struct definition
+    tOptions = struct;
+        tOptions.theta = theta;
+        tOptions.method = method;
+
+        if not(isequal(method,'newton')) 
+            tOptions.submethod = [];
+            
+            if not(isempty(mode)) || not(isempty(toll)) || not(isempty(nmax))
+                warning('some input may be unused for the current method selected')
+            end
+        else
+            tOptions.submethod = struct;
+                tOptions.submethod.mode = mode;
+                tOptions.submethod.toll = toll;
+                tOptions.submethod.nmax = nmax;
+        end
         
 end
-
--- Assignment 1/Code/src/theta.m
+++ Assignment 1/Code/src/theta.m
@@ -1,108 +1,142 @@
 function [x,t,info] = theta(f,x0,tmax,h,tOptions,visualConfig)
-%THETA Summary of this function goes here
-%   Detailed explanation goes here
-
-%%% Optional input definition
-if nargin < 6
-    visualConfig.plot = true;
-end
-
-if nargin < 5
-tOptions = struct;
-    tOptions.theta = [];
-    tOptions.method = [];
-    tOptions.submethod = [];
-        tOptions.submethod.mode = [];
-        tOptions.submethod.toll = [];
-        tOptions.submethod.nmax = [];
-end
-
-
-%%% Dimension Check for initial guess
-if size(x0,2) > 1
-    error('The initial guess is invalid, too many input in x0 vector compare to the order selected\n')
-end
-
-
-%%% Default method
-if isempty(tOptions.method)
-    tOptions.method = 'fsolve';      % default implicit solver
-end
-
-if isempty(tOptions.theta)
-    tOptions.theta = 0.5;            % default theta value
-end
-
-                   
-%%% Initialization
-timerStart = tic;               % timer start
-feval = 0;                      % function evaluation counter starts
-dimSys = size(x0,1);            % function evaluation step
-t = 0:h:tmax;                   % time vector definition
-thetaParam = tOptions.theta;
-x = [x0 zeros(dimSys,length(t)-1)];    % solution vector allocation
+%THETA - Theta method application
+%
+%   Syntax:
+%       [x,t,info] = theta(f,x0,tmax,h,tOptions,visualConfig)
+%
+%   Input:
+%       f,       function(x,t):  IVP problem
+%       x0,        double[n,1]:  generic initial guess 
+%       tmax,           double:  upper time limit of the integration
+%       h,              double:  time step of the integration
+%       tOtpions(*),    struct:  see tSettings.m for details
+%       visualConfig(*),  bool:  set as true to plot solutions 
+%
+%   Output:
+%       x,     double[n,m]:  solution vector
+%       t,     double[1,m]:  time istant associated to solutions
+%       info,       struct:  information on method used:
+%           - info.timeCost,     double:  time spent
+%           - info.fevalCost,    double:  # of function evaluations
+%           - info.fvalVec, dobule[n,m]:  f evaluated in solution points
+%           - info.implicit,       bool:  true if the method is implicit
+%
+%   Default settings for optional input (*):
+%       tOtpions:      set with default solver 'fsolve' and 'theta = 0.5'
+%       visualConfig:  set as true by default
+%
 
 
-%%% THETA loop
-for i = 1 : (length(t)-1)         % main loop of the method
-    tk = t(i);
-    xk = x(:,i);
-    xinter = xk + thetaParam * h * (f(xk,tk));       % x_k+1/2
+    %%% Optional input definition
+    if nargin < 6
+        visualConfig = true;
+    end
+
+    if nargin < 5
+    tOptions = struct;
+        tOptions.theta  = [];
+        tOptions.method = [];
+    end
+
+
+    %%% Dimension Check for initial guess
+    if size(x0,2) > 1
+        error('The initial guess is invalid, too many input in x0 vector compare to the order selected')
+    end
+
+    if size(x0,1) > 1 && not(isequal(tOptions.method,'fsolve'))
+        error('The method select currently do not support multivariable system')
+    end
+
+
+    %%% Default method
+    if isempty(tOptions.theta)
+        tOptions.theta = 0.5;            % default theta value
+    end
+
+    if isempty(tOptions.method)
+        tOptions.method = 'fsolve';      % default implicit solver
+    end
+
+                    
+    %%% Initialization
+    timerStart = tic;               % timer start
+    feval = 0;                      % function evaluation counter starts
+    dimSys = size(x0,1);            % function evaluation step
+    t = 0:h:tmax;                   % time vector definition
+
+    thetaParam = tOptions.theta;
+    x = [x0 zeros(dimSys,length(t)-1)];             % solution vector allocation
+    fvalVec = [f(x(:,1),t(1)), ...
+                    zeros(dimSys,length(t)-1)];     % fval vector allocation
+    feval = feval + dimSys;                         % function evaluation counter update
+
+
+    %%% THETA loop
+    for i = 1 : (length(t)-1)                   % main loop of the method
+        tk = t(i);
+        xk = x(:,i);
+        fk = fvalVec(:,i);                      % fk = f(xk,tk)
+        xinter = xk + thetaParam * h * fk;      % x_k+1/2
+        
+        tn = tk + h;
+        thetaFunction = @(xn) xn - xk - h * thetaParam * fk - (1-thetaParam) * h * f(xn,tn) ;
+        
+        switch tOptions.method
+            case 'fsolve'
+                % implicit equation solver
+                options = optimoptions ( 'fsolve', 'Display', 'off' );
+                [xn,fvalVec(:,i+1),conv,info] = fsolve(thetaFunction, xinter, options);      % works in both scalar and vectorial cases
+
+                if conv < 1
+                    warning('Implicit equation at the step %d has not been solved correctly',i)
+                end
+                feval = feval + info.funcCount;
+
+            case 'fzero'
+                % implicit equation solver
+                [xn,fvalVec(:,i+1),conv,info] = fzero(thetaFunction, xinter);                % works only in scalar case
+                
+                if conv < 1
+                    warning('Implicit equation at the step %d has not been solved correctly',i)
+                end
+                feval = feval + info.funcCount;
+
+            case 'newton'
+                if isempty(tOptions.submethod.mode)
+                    tOptions.submethod.mode = 'f';        % default implicit solver options
+                end
+                
+                % implicit equation solver
+                [xn,conv,info] = newton(thetaFunction, xinter, tOptions.submethod.mode, ...
+                                        tOptions.submethod.toll, tOptions.submethod.nmax);   % works only in scalar case for the moment
+                if conv == 0 
+                    warning('Implicit equation at the step %d has not been solved correctly',i)
+                end
+                fvalVec(:,i+1) = f(xn,t(i+1));
+                feval = feval + info.fevalcost + dimSys;
+
+            otherwise
+                error('Insert a valid method as input');
+        end  
     
-    tn = tk + h;
-    thetaFunction = @(xn) xn - xk - h * thetaParam * f(xk,tk) - (1-thetaParam) * h * f(xn,tn) ;
-    feval = feval + 2*dimSys;
-    
-    switch tOptions.method
-        case 'fsolve'
-            % implicit equation solver
-            options = optimoptions ( 'fsolve', 'Display', 'off' );
-            [xn,~,conv,info] = fsolve(thetaFunction, xinter, options);    % works in both scalar and vectorial cases
+        x(:,i+1) = xn;              % solution value in solution vector
+    end
 
-            if conv < 1
-                warning('Implicit equation at the step %d has not been solved correctly',i)
-            end
-            feval = feval + info.funcCount;
+    elapsedTime = toc(timerStart);  % timer stop
 
-        case 'fzero'
-            % implicit equation solver
-            [xn,~,conv,info] = fzero(thetaFunction, xinter);              % works only in scalar case
-            
-            if conv < 1
-                warning('Implicit equation at the step %d has not been solved correctly',i)
-            end
-            feval = feval + info.funcCount;
-
-        case 'newton'
-            if isempty(tOptions.submethod.mode)
-                tOptions.submethod.mode = 'f';        % default implicit solver options
-            end
-            
-            % implicit equation solver
-            [xn,conv,info] = newton(thetaFunction, xinter, tOptions.submethod.mode, ...
-                                    tOptions.submethod.toll, tOptions.submethod.nmax);    % works only in scalar case for the moment
-            if conv == 0 
-                warning('Implicit equation at the step %d has not been solved correctly',i)
-            end
-            feval = feval + info.fevalcost;
-
-        otherwise
-            error('Insert a valid method as input\n');
-    end  
- 
-    x(:,i+1) = xn;      % solution value in solution vector
-end
-
-elapsedTime = toc(timerStart);   % timer stop
-
-info = struct;
-    info.timeCost = elapsedTime;
-    info.fevalCost = feval;
+    if nargout == 3
+        info = struct;              % info struct build-up
+            info.timeCost  = elapsedTime;
+            info.fevalCost = feval;
+            info.fvalVec   = fvalVec;
+            info.implicit  = true;
+    end
 
 
-%%% plot
-if visualConfig.plot == true
-    plot(t,x,'o-');
-end
+    %%% plot
+    if visualConfig == true
+        plot(t,x,'o-');
+    end
 
 end
