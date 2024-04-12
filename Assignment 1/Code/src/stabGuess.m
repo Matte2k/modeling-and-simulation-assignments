db5b2534d0183d25b46a9cf2656e45ed1ea18a63
Matte2k <matteob2000@hotmail.it>
2024-04-12 13:23:03 +0200

    Cosmetic review of stability guess function [stabGuess.m]

--- Assignment 1/Code/src/stabGuess.m
+++ Assignment 1/Code/src/stabGuess.m
@@ -1,21 +1,24 @@
-function []=stabGuess(F,degStart,hVec)
+function [] = stabGuess(F,degStart,hVec)
+%STAB GUESS - Plot the Stability problem function in the starting alpha
 %
+%   Syntax:
+%       [] = stabGuess(F,degStart,hVec)
 %
-%
-%
+%   Input:
+%       F,   function(h,A):  corresponding method operator
+%       degStart,   double:  starting point of the stability analysis
+%       hVec,  double[1,2]:  starting and ending h used in fplot
 %
 %     
 
 radStart = deg2rad(degStart);
 A = [0, 1; -1, 2*cos(radStart)];
-S = @(h) max(abs(eig(F(h,A)))) - 1;
+S = @(h) max(abs(eig(F(h,A)))) - 1;     % Stability problem definition
 
-fplot(S,hVec, Color = 'k');      % usare per initial guess
-hold on
-yline(0, LineStyle = '--')
-ylim([-1 5])
-grid on;    axis padded;    box on;
-xlabel('$h$');      ylabel('$S(h)$')      % where S is the stability Slem function
+fplot(S,hVec, Color = 'k');             % usare per initial guess
+hold on;    grid on;    axis padded;    box on; 
+ylim(hVec);       yline(0, LineStyle = '--')
+xlabel('$h$');    ylabel('$S(h)$')      % where S is the stability function
 tstring = sprintf('Stability function in alpha = %.0f',degStart);
 title (tstring);
 drawnow
